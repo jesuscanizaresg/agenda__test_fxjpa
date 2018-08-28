85e9179a52454e5e6f23d990fdf513ccce145dcd
DESKTOP-NF3UADG\Jesus-Pc <jesus.canizares@qualityprovider.net>
2018-08-28 12:03:50 +0200

    first Commit, only testing

---
+++ src/aplicacionAgenda/SampleController.java
@@ -0,0 +1,20 @@
+package aplicacionAgenda;
+
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class SampleController {
+
+
+	@FXML
+	void insertarActor() {
+		System.out.println("Hola");
+	}
+	
+	@FXML
+	void saludar() {
+		System.out.println("Saludando");
+	}
+
+}
